de58684a740cf8be803a6fadc01be6a7855eaed5
P0ZITIV <66378765+P0ZITIV-iwns@users.noreply.github.com>
2023-09-13 19:18:35 +0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,17 +1,26 @@
 <?php
-function format($price){
+const SECONDS_IN_MINUTE = 60;
+function format($price): string
+{
     return number_format($price, 0, '.', ' ').' ₽';
 }
 
-function timeLeft($dateEnd){
-    $hours = floor((strtotime($dateEnd) - time()) / 3600);
-    $minutes = floor(((strtotime($dateEnd) - time()) % 3600) / 60);
+function timeLeft($dateEnd): string
+{
+    $hours = floor((strtotime($dateEnd) - time()) / pow(SECONDS_IN_MINUTE, 2));
+    if ($hours < 9) {
+        $hours = str_pad($hours, 2, '0', STR_PAD_LEFT);
+    }
+    $minutes = floor(((strtotime($dateEnd) - time()) / SECONDS_IN_MINUTE) % SECONDS_IN_MINUTE);
+    if ($minutes < 9){
+        $minutes = str_pad($minutes, 2, '0', STR_PAD_LEFT);
+    }
     return "{$hours}:{$minutes}";
 }
 
-function add_style($dateEnd)
+function add_style($dateEnd): string
 {
-    $hours = floor((strtotime($dateEnd) - time()) / 3600);
+    $hours = floor((strtotime($dateEnd) - time()) / SECONDS_IN_MINUTE**2);
     $isAddStyle = $hours < 1;
-    return $isAddStyle ? "timer--finishing" : "";
+    return $isAddStyle ? 'timer--finishing' : '';
 }
